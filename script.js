7aed6bacf9feca720198783900b7195039719213
Christopher Silva <109239723+chris-oliver-git@users.noreply.github.com>
2025-05-17 22:12:41 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const botao = document.querySelector("#botao");
-let mensagem = document.querySelector("#mensagem");
-
-
-    function mostrarMensagem() {
-        mensagem.textContent = 'Você é incrível! Continue firme no seu caminho.';
-    }
-    
-    botao.addEventListener("click", mostrarMensagem);
